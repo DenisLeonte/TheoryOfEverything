9c436943116644caa75de4cf25c6c199388f90a1
MyBoyDoru <denis.leonte123@gmail.com>
2020-02-07 18:18:16 +0200

    Reading is done

--- Noduri/Project1/main.cpp
+++ Noduri/Project1/main.cpp
@@ -2,14 +2,65 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#define NMAX 500
 
-void read(std::string filename, std::string typeof)
+using namespace std;
+int n, m;
+int a[NMAX][NMAX];
+
+void type1(string filename)
 {
-	std::ifstream in(filename.c_str());
+	ifstream in(filename.c_str());
+	in >> n;
+	int x;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			in >> x;
+			a[i][j] = x;
+		}
+	}
+	in.close();
+}
 
+void type2(string filename)
+{
+	ifstream in(filename.c_str());
+	in >> n >> m;
+	int x, y;
+	for (int i = 0; i < m; i++)
+	{
+		in >> x >> y;
+		a[x][y] = 1;
+		a[y][x] = 1;
+	}
+	in.close();
+}
+
+void read(string filename, string typeof)
+{
+	if (typeof.compare("adiacenta") == 0)
+		type1(filename);
+	else if (typeof.compare("muchii") == 0)
+		type2(filename);
+}
+
+void print()
+{
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			cout<<a[i][j]<<" ";
+		}
+		cout << endl;
+	}
 }
 
 int main()
 {
+	read("mat_lat.txt", "muchii");
+	print();
 	return 0;
 }
