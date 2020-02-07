a12776cbca5d70566c676649a7928182c1692c9f
MyBoyDoru <denis.leonte123@gmail.com>
2020-02-07 21:11:41 +0200

    read and rsd

--- Arbori/Arbori/main.cpp
+++ Arbori/Arbori/main.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#define NMAX 100
 
-void read()
+using namespace std;
+
+ifstream in("Text.txt");
+
+int rad;
+int n;
+int t[NMAX];
+int st[NMAX], dr[NMAX];
+int val[NMAX];
+
+void read(int currentNode)
 {
-	std::ifstream in("Text.txt");
+	int x;
+	in >> x;
+	st[currentNode] = x;
+	if (x != 0)
+		read(x);
+	int y;
+	in >> y;
+	dr[currentNode] = y;
+	if (y != 0)
+		read(y);
+}
+
+void rsd(int x)
+{
+	if (x)
+	{
+		cout << x << " ";
+		rsd(st[x]);
+		rsd(dr[x]);
+	} 
 }
 
 int main()
 {
-
+	in >> rad;
+	read(rad);
+	for (int i = 1; i <= n; i++)
+	{
+		t[st[i]] = i;
+		t[dr[i]] = i;
+	}
+	rsd(rad);
 }
