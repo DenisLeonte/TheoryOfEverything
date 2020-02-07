38f63319d7e0114326dffa2776143d9d0bb193b1
MyBoyDoru <denis.leonte123@gmail.com>
2020-02-07 17:59:26 +0200

    Try One

---
+++ Noduri/Project1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+void read(std::string filename, std::string typeof)
+{
+	std::ifstream in(filename.c_str());
+
+}
+
+int main()
+{
+	return 0;
+}
