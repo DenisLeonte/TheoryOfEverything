53b96e77e540afa701b5f0ac789b9a51059e0d2b
MyBoyDoru <denis.leonte123@gmail.com>
2020-02-12 18:04:58 +0200

    V1.0

---
+++ Nodes/Nodes/main.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct produs
+{
+	string name;
+	float cost;
+	float mkCost;
+	int depStock;
+	int storeStock;
+};
+
+struct nod
+{
+	produs x;
+	nod* urm;
+} *prim, * ultim;
+
+void creare()
+{
+	ifstream in("data.in");
+	produs y;
+	while (in >> y.name >> y.cost >> y.mkCost >> y.depStock >> y.storeStock)
+	{
+		nod* nou = new nod;
+		nou->x = y;
+		nou->urm = NULL;
+		if (prim == NULL)
+		{
+			prim = ultim = nou;
+		}
+		else
+		{
+			ultim->urm = nou;
+			ultim = nou;
+		}
+	}
+}
+
+void nMatch(string x)
+{
+	nod* p = prim;
+	while (p != NULL)
+	{
+		if (p->x.name == x)
+		{
+			cout << p->x.name << " " << p->x.cost << " " << p->x.mkCost << " " << p->x.storeStock << " " << p->x.depStock << endl;
+		}
+		p = p->urm;
+	}
+}
+
+void ASort()
+{
+	cout << "Lista alfabetica la produsele :" << endl;
+	vector<string> aux;
+	nod* p = prim;
+	while (p != NULL)
+	{
+		aux.push_back(p->x.name);
+		p = p->urm;
+	}
+	sort(aux.begin(), aux.end());
+	for (auto it : aux)
+	{
+		nMatch(it);
+	}
+}
+
+void profit()
+{
+	cout << "Lista cu profitul fiecarui produs :" << endl;
+	nod* p = prim;
+	while (p != NULL)
+	{
+		cout << p->x.name << " " << p->x.cost - p -> x.mkCost << endl;
+		p = p->urm;
+	}
+}
+
+void unReleased()
+{
+	cout << "Lista cu produsele nelansate :" << endl;
+	nod* p = prim;
+	while (p != NULL)
+	{
+		if (p->x.storeStock == 0 && p->x.depStock == 0)
+		{
+			cout << p->x.name << " " << p->x.cost << endl;
+		}
+		p = p->urm;
+	}
+}
+
+int ProfitPerProd(nod* x)
+{
+	return x->x.cost * x->x.storeStock + x->x.mkCost * x->x.depStock;
+}
+
+void profitTotal()
+{
+	cout << "Lista cu profitele totale :"<<endl;
+	nod* p = prim;
+	while (p != NULL)
+	{
+		cout << p->x.name << " " << ProfitPerProd(p) << endl;
+		p = p->urm;
+	}
+}
+
+int main()
+{
+	creare();
+	//ASort();
+	//profit();
+	//unReleased();
+	profitTotal();
+	return 0;
+}
