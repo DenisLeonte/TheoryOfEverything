a43525f5ac3eb875a767b6414ad9ca083d3224ee
MyBoyDoru <denis.leonte123@gmail.com>
2020-02-17 18:07:28 +0200

    V2.0

--- summax/summax/main.cpp
+++ summax/summax/main.cpp
@@ -10,29 +10,56 @@ ofstream out("summax.out");
 int n;//Numar de linii
 vector <int> a[2000];
 int k = 0;
-int Max;
+int Max = 0;
 
 int move(int i, int j)
 {
-	if (i == n-1)
+	if (i == n)
 		return a[i][j];
 	else
 	{
 		int s1 = move(i + 1, j);
 		int s2 = move(i + 1, j + 1);
-		if (s1 == Max)
+		if (s1 >= s2)
+			return s1 + a[i][j];
+		else
+			return s2 + a[i][j];
+	}
+}
+
+int summax(int i, int j)
+{
+	if (i == n)
+		return a[i][j];
+	else
+	{
+		int s1 = summax(i + 1, j);
+		int s2 = summax(i + 1, j + 1);
+		if (s1 + a[i][j] >= Max)
 			k++;
-		if (s2 == Max)
+		if (s2 + a[i][j] >= Max)
 			k++;
 		if (s1 >= s2)
-			return s1+a[i][j];
+			return s1 + a[i][j];
 		else
-			return s2+a[i][j];
+			return s2 + a[i][j];
+	}
+}
+
+void afis()
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (auto it : a[i])
+		{
+			cout << it << " ";
+		}
+		cout << endl;
 	}
 }
 
 int main()
-{	
+{
 	int v, st, dr;
 	in >> v >> n >> st >> dr;
 	for (int i = 0; i <= n; i++)
@@ -46,7 +73,7 @@ int main()
 	}
 	Max = move(0, 0);
 	k = 0;
-	int a = move(0, 0);
+	int a = summax(0, 0);
 	cout << k;
 	return 0;
 }
